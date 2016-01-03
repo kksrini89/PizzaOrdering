73058eed4eaa228f52af3e6792b09c780a94f801
kksrini89 <kksrini89@gmail.com>
2016-01-03 22:35:25 +0530

    Missed files are added.

---
+++ Kks.Pizza/App/app.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    "use strict";
+    var app = angular.module('pizzaApp', ['ngResource', 'ui.router', 'ui.bootstrap']);
+    app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+        $stateProvider.state('Home', {
+            url: '/',
+            templateUrl: 'App/MainPage.html'
+        })
+        .state('Menu', {
+            url: '/menu',
+            templateUrl: 'App/Products/partials/product.html',
+            controller: 'productController'
+        })
+        .state('CustomerDetails', {
+            url: '/customerdetails',
+            templateUrl: 'App/Order/partials/order.html',
+            controller: 'orderController'
+        })
+        .state('End', {
+            url: '/end',
+            templateUrl: 'App/End/partials/end.html',
+            controller: 'endController'
+        });
+    }]);
+
+    app.constant('appSettings', {
+        serverPath: "http://localhost:29208"
+    });
+})();
