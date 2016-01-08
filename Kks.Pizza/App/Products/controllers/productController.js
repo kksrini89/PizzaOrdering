4eeb6238051d61a6ca3cc01ef7f223e0942fa62f
kksrini89 <kksrini89@gmail.com>
2016-01-08 23:04:59 +0530

    Toaster library added for Notification-Add to cart

--- Kks.Pizza/App/Products/controllers/productController.js
+++ Kks.Pizza/App/Products/controllers/productController.js
@@ -2,6 +2,7 @@
     'use strict';
     angular.module('pizzaApp')
             .controller('productController', ['$scope', 'productResource', '$state', function ($scope, productResource, $state) {
+
                 $scope.products = [];
                 $scope.products = productResource.getProducts();
                 $scope.selectedItemCount = 0;
@@ -9,13 +10,69 @@
                 $scope.selectedItems = function (item) {
                     if (item != null) {
                         if (temp.length == 0) {
-                            temp.push(item);
+                            temp.push(item);                            
+                            toastr.success('added', item.productName);
+                            toastr.options = {
+                                "closeButton": false,
+                                "debug": false,
+                                "newestOnTop": false,
+                                "progressBar": false,
+                                "positionClass": "toast-top-right",
+                                "preventDuplicates": false,
+                                "onclick": null,
+                                "showDuration": "300",
+                                "hideDuration": "1000",
+                                "timeOut": "5000",
+                                "extendedTimeOut": "1000",
+                                "showEasing": "swing",
+                                "hideEasing": "linear",
+                                "showMethod": "fadeIn",
+                                "hideMethod": "fadeOut"
+                            }
                             $scope.selectedItemCount++;
                         }
                         else {
                             if ((temp.indexOf(item)) < 0) {
                                 temp.push(item);
+                                toastr.success('added', item.productName);
+                                toastr.options = {
+                                    "closeButton": false,
+                                    "debug": false,
+                                    "newestOnTop": false,
+                                    "progressBar": false,
+                                    "positionClass": "toast-top-right",
+                                    "preventDuplicates": false,
+                                    "onclick": null,
+                                    "showDuration": "300",
+                                    "hideDuration": "1000",
+                                    "timeOut": "1000",
+                                    "extendedTimeOut": "1000",
+                                    "showEasing": "swing",
+                                    "hideEasing": "linear",
+                                    "showMethod": "fadeIn",
+                                    "hideMethod": "fadeOut"
+                                }
                                 $scope.selectedItemCount++;
+                            }
+                            else {
+                                toastr.error('Already added', item.productName);
+                                toastr.options = {
+                                    "closeButton": false,
+                                    "debug": false,
+                                    "newestOnTop": false,
+                                    "progressBar": false,
+                                    "positionClass": "toast-top-right",
+                                    "preventDuplicates": false,
+                                    "onclick": null,
+                                    "showDuration": "10",
+                                    "hideDuration": "10",
+                                    "timeOut": "1000",
+                                    "extendedTimeOut": "1000",
+                                    "showEasing": "swing",
+                                    "hideEasing": "linear",
+                                    "showMethod": "fadeIn",
+                                    "hideMethod": "fadeOut"
+                                }
                             }
                         }
                     }
