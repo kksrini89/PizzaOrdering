a1eecb7662438c12a20e6823e47b48f196d54f26
kksrini89 <kksrini89@gmail.com>
2016-01-09 20:23:42 +0530

    Toastr added - Order placed notification.

--- Kks.Pizza/App/Order/controllers/orderController - Copy.js
+++ Kks.Pizza/App/Order/controllers/orderController - Copy.js
@@ -1,6 +1,14 @@
 ﻿(function () {
     angular.module('pizzaApp')
     .controller('orderController', ['$scope', '$state', '$stateParams', 'orderResource', function ($scope, $state, $stateParams, orderResource) {
+        //var products = [];
+        //products = $stateParams.products;
+        //var viewmodel = {};
+        //viewmodel.customerName = "";
+        //viewmodel.mobileNumber = "";
+        //viewmodel.address = "";
+        //$scope.model = viewmodel;        
+
         var viewModel = {
             customer: {
                 name: "",
@@ -19,7 +27,7 @@
             if (order != null) {
                 orderResource.saveItem(order)
                 .$promise.then(function (data) {
-                    //console.log("Success", data);
+                    console.log("Success", data);
                     if (data.products.length > 1) {
                         toastr.success('Your orders are placed');
                     }
@@ -36,8 +44,8 @@
                         "onclick": null,
                         "showDuration": "300",
                         "hideDuration": "1000",
-                        "timeOut": "3000",
-                        "extendedTimeOut": "4000",
+                        "timeOut": "1000",
+                        "extendedTimeOut": "2000",
                         //for animation
                         "showEasing": "swing",
                         "hideEasing": "linear",
@@ -47,10 +55,7 @@
                     }
                     $state.go('End', { customer: order.customer });
                 },
-                function (failure) {
-                    //console.log("failure", failure);
-                    toastr.error("Try after sometime. Sorry for the inconvenience");
-                });
+                function (failure) { console.log("failure", failure) });
             };
         }
     }]);
--- Kks.Pizza/App/Products/controllers/productController - Copy.js
+++ Kks.Pizza/App/Products/controllers/productController - Copy.js
@@ -8,56 +8,74 @@
                 var temp = [];
                 $scope.selectedItems = function (item) {
                     if (item != null) {
-                        if (temp !== undefined) {
+                        if (temp.length == 0) {
+                            temp.push(item);                            
+                            toastr.success('added', item.productName);
+                            toastr.options = {
+                                "closeButton": false,
+                                "debug": false,
+                                "newestOnTop": false,
+                                "progressBar": false,
+                                "positionClass": "toast-top-right",
+                                "preventDuplicates": false,
+                                "onclick": null,
+                                "showDuration": "300",
+                                "hideDuration": "1000",
+                                "timeOut": "5000",
+                                "extendedTimeOut": "1000",
+                                "showEasing": "swing",
+                                "hideEasing": "linear",
+                                "showMethod": "fadeIn",
+                                "hideMethod": "fadeOut"
+                            }
+                            $scope.selectedItemCount++;
+                        }
+                        else {
                             if ((temp.indexOf(item)) < 0) {
                                 temp.push(item);
-                                toastr.info('added', item.productName);
+                                toastr.success('added', item.productName);
                                 toastr.options = {
                                     "closeButton": false,
                                     "debug": false,
-                                    "newestOnTop": true,
+                                    "newestOnTop": false,
                                     "progressBar": false,
                                     "positionClass": "toast-top-right",
                                     "preventDuplicates": false,
                                     "onclick": null,
-                                    "showDuration": "10000",
+                                    "showDuration": "300",
                                     "hideDuration": "1000",
                                     "timeOut": "1000",
-                                    "extendedTimeOut": "2000",
-                                    //for animation
-                                    "showEasing": "swing",
-                                    "hideEasing": "linear",
-                                    //for showing
-                                    "showMethod": "fadeIn",
-                                    "hideMethod": "fadeOut"
-                                }
-                                $scope.selectedItemCount++;
-                            }
-                            else {
-                                toastr.options = {
-                                    "closeButton": false,
-                                    "closeHtml": '<button><i class="icon-off"></i></button>',
-                                    "debug": false,
-                                    "newestOnTop": true,
-                                    "progressBar": false,
-                                    "positionClass": "toast-top-right",
-                                    "preventDuplicates": true,
-                                    "onclick": null,
-                                    "showDuration": "10",
-                                    "hideDuration": "10",
-                                    "timeOut": "600",
                                     "extendedTimeOut": "1000",
                                     "showEasing": "swing",
                                     "hideEasing": "linear",
                                     "showMethod": "fadeIn",
                                     "hideMethod": "fadeOut"
                                 }
-                                toastr.error('Already added');
+                                $scope.selectedItemCount++;
+                            }
+                            else {
+                                toastr.error('Already added', item.productName);
+                                toastr.options = {
+                                    "closeButton": false,
+                                    "debug": false,
+                                    "newestOnTop": false,
+                                    "progressBar": false,
+                                    "positionClass": "toast-top-right",
+                                    "preventDuplicates": false,
+                                    "onclick": null,
+                                    "showDuration": "10",
+                                    "hideDuration": "10",
+                                    "timeOut": "1000",
+                                    "extendedTimeOut": "1000",
+                                    "showEasing": "swing",
+                                    "hideEasing": "linear",
+                                    "showMethod": "fadeIn",
+                                    "hideMethod": "fadeOut"
+                                }
                             }
                         }
                     }
-                }
-
+                };
                 $scope.OnNextClick = function () {
                     if (angular.isDefined(temp) && temp.length > 0 && $scope.selectedItemCount > 0)
                         $state.go('CustomerDetails', { products: temp });
