d3f98cfdda6f4e42ae95a11cee8b1c6988eb6fe8
kksrini89 <kksrini89@gmail.com>
2016-01-22 08:29:22 +0530

    Login form validation done.

--- Kks.Pizza/App/Login/controllers/loginController.js
+++ Kks.Pizza/App/Login/controllers/loginController.js
@@ -1,17 +1,25 @@
 ﻿(function () {
     angular.module('pizzaApp')
-           .controller('loginController', ['$scope', function ($scope) {
+           .controller('loginController', ['$scope', '$state', function ($scope, $state) {
                var viewModel = {
                    userName: "",
-                   password: ""
+                   password: "",
+                   onSubmit: function () {
+                       //Get the Previous ui route state to navigate. Logged In user will be displayed at the top of the screen.
+                       //$state.go('previous state');
+                   },
+                   onCancel: function () {
+                       //Get the Previous ui route state to navigate.
+                       //$state.go('previous state');
+                   }
                };
 
-               viewModel.OnSubmit = function () {
+               //viewModel.OnSubmit = function () {
 
-               };
-               viewModel.OnCancel = function () {
+               //};
+               //viewModel.OnCancel = function () {
 
-               };
+               //};
                $scope.entity = viewModel;
            }]);
 })();
