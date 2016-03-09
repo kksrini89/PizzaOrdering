ae1d67cd4ece09ede7bfcd622bd04b7221fefb5b
srinivasan <kksrini89@gmail.com>
2016-03-09 22:43:52 +0530

    LoggedInUser Name is shown

--- Kks.Pizza/App/Main/controllers/rootController.js
+++ Kks.Pizza/App/Main/controllers/rootController.js
@@ -2,10 +2,12 @@
     'use strict';
     angular.module('pizzaApp')
            .controller('rootController', ['$location', '$scope', 'auth', function ($location, $scope, auth) {
+               $scope.auth = {};
                $scope.isUserLoggedIn = false;
                $scope.login = function () {
                    auth.signin({}, function (profile, token) {
                        $location.path('/menu');
+                       $scope.auth = auth;
                        $scope.isUserLoggedIn = true;
                    }, function (error) {
                        console.log(error);
